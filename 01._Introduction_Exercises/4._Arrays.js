7d24d7c1bd709243bc6f8c1dcde406cdfe9a159f
phaberen <magnusphaberen@gmail.com>
2026-09-03 23:51:33 +0200

    Intoduction exercises finished

--- 01._Introduction_Exercises/4._Arrays.js
+++ 01._Introduction_Exercises/4._Arrays.js
@@ -4,25 +4,30 @@
 // Exercise 1 - Array Positioning
 
 const letters = ["a", "b", "c"];
+
 // show b in the console 
 
+console.log(letters[1]);
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
 
 const friends = [];
 
-// What a lonely array. Add at least 3 friend objects to it.  
+friends.push({ name: "Magnus", age: 30}, { name: "Ole", age: 40}, { name: "Jens", age: 25} );
+console.log(friends);
 
+// What a lonely array. Add at least 3 friend objects to it.  
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
 
 const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
+console.log(significantMathNumbers.indexOf(1729));
+
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
-
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -31,25 +36,25 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
-
-
+diet.splice(2, 0, "hamburger", "soda", "pizza");
+console.log(diet);
 
 // --------------------------------------
 // Exercise 5 - Remove element
 
 // Remove the LAST element of the array.
-// Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
+// Don"t remove by index. You know in advance that it"s the last in the array because you are too full already. 
 
-
-
-
+diet.pop();
+console.log(diet);
 
 // --------------------------------------
 // Exercise 6 - Copy array
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = diet.slice(0, diet.length);
+console.log("This is the dinnerTray -> ", dinnerTray);
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -58,7 +63,9 @@ const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
-
+for (let i = 1; i < lettersExpanded.length; i+=2) {
+    console.log(lettersExpanded[i]);
+} 
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -69,6 +76,16 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0 || numbers[i] > 6) {
+        console.log(numbers[i]);
+    }
+    else {
+        discardedNumbers.push(numbers[i]);
+    }
+}
+console.log(discardedNumbers);
 
 // --------------------------------------
 
